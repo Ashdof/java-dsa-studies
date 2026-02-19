3acb60ebd55ea8b40a593af0fc00321e9fd90709
Emmanuel Enchill <mzigah4@gmail.com>
2026-02-19 16:38:58 +0000

    Implemented the iterative form of the Euclid's gcd algorithm

--- src/com/dsa_lessons/chapter_4/EuclideGCDDemo.java
+++ src/com/dsa_lessons/chapter_4/EuclideGCDDemo.java
@@ -3,8 +3,8 @@ package com.dsa_lessons.chapter_4;
 public class EuclideGCDDemo {
 	public static void main(String[] args) {
 		
-		System.out.printf("GCD of %d and %d: %d%n", 1680, 640, gcd(1680, 640));
-		System.out.printf("GCD of %d and %d: %d%n", 1680, 640, gcd(48, 18));
+		System.out.printf("GCD of %d and %d: %d%n", 1680, 640, gcdInterative(1680, 640));
+		System.out.printf("GCD of %d and %d: %d%n", 1680, 640, gcdInterative(48, 18));
 	}
 	
 	private static int gcd(int a, int b) {
@@ -15,5 +15,15 @@ public class EuclideGCDDemo {
 		
 		// recursive case
 		return gcd(b, a % b);
+	}
+
+	private static int gcdInterative(int a, int b) {
+		while (b != 0) {
+			int temp = b;
+			b = a % b ;
+			a = temp;
+		}
+		
+		return a;
 	}
 }
